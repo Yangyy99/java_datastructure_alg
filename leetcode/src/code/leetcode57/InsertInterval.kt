072c87d8dd2ab8ba8a7c504bef292b6e7ba20ae5
yunxiao <2429438373@qq.com>
2022-03-26 17:51:54 +0800

    排序法 结合leetcode56 解决插入区间

--- leetcode/src/code/leetcode57/InsertInterval.kt
+++ leetcode/src/code/leetcode57/InsertInterval.kt
@@ -1,5 +1,7 @@
 package code.leetcode57
 
+import java.util.*
+
 /**
  * @Author: 云萧YYY
  * @DateTime: 2022/03/25
@@ -8,9 +10,77 @@ package code.leetcode57
  */
 
 fun main() {
+    //intervals = [[1,3],[6,9]], newInterval = [2,5]
+    //intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8
+    val intervals = arrayOf(intArrayOf(1, 2), intArrayOf(3, 5), intArrayOf(6, 7), intArrayOf(8, 10), intArrayOf(12, 16))
+    val newInterval = intArrayOf(4, 8)
+
+    insert(intervals, newInterval).forEach {
+        it.contentToString().apply(::println)
+    }
+}
+
+/***
+ * 贪心算法，解决合并区间，在插入时解决
+ */
+fun greedyInsert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+    return arrayOf()
+}
+
+/***
+ * 插入区间  排序法 结合leetcode 56
+ * 新增的区间先放入最后, 在合并区间
+ */
+fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+
+    val newArray = copy(intervals)
+    newArray[intervals.size] = newInterval
+    Arrays.sort(newArray) { o1, o2 -> o1[0] - o2[0] };
+    return mergeInterval(newArray);
 
 }
-//fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
-//
-//
-//}
+
+/***
+ * 区间合并leetcode56,这里只合并
+ * @param intervals 有序的数组
+ */
+private fun mergeInterval(intervals: Array<IntArray>): Array<IntArray> {
+
+    if (intervals.size < 2) return intervals
+
+    val result = Array<IntArray>(intervals.size) {
+        IntArray(it)
+    }
+    var index = 0
+    result[index++] = intArrayOf(intervals[0][0], intervals[0][1])
+
+    for (i in 1 until intervals.size) {
+
+        val left = intervals[i][0];
+        val right = intervals[i][1];
+        /**
+         * 要合并的区间
+         */
+        if (result[index - 1][1] in (left) until right) result[index - 1][1] = right
+        /**
+         * 不需要合并 直接放入result
+         */
+        if (left > result[index - 1][1]) {
+            result[index++] = intArrayOf(left, right)
+        }
+    }
+
+    return result.copyOfRange(0, index);
+}
+
+/**
+ * copy数组
+ */
+private fun copy(old: Array<IntArray>): Array<IntArray> {
+
+    val newArray = Array<IntArray>(old.size + 1) {
+        intArrayOf(it)
+    }
+    System.arraycopy(old, 0, newArray, 0, old.size);
+    return newArray
+}
